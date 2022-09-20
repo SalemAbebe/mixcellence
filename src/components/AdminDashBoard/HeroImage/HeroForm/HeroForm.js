6017b2f9f8c87b24e1d1d6489245e080c9be77ad
aim4dabush <edarasato@gmail.com>
2022-09-20 15:17:30 -0700

    fixed HeroForm error

--- src/components/AdminDashBoard/HeroImage/HeroForm/HeroForm.js
+++ src/components/AdminDashBoard/HeroImage/HeroForm/HeroForm.js
@@ -60,6 +60,7 @@ function HeroForm() {
         <input
           type="text"
           id="hero-heading"
+          name="heading"
           value={formInfo.heading}
           onChange={onChangeHandler}
         />
@@ -69,6 +70,7 @@ function HeroForm() {
         <input
           type="text"
           id="hero-sub-heading"
+          name="subHeading"
           value={formInfo.subHeading}
           onChange={onChangeHandler}
         />
