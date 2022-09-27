11af22d809aff2e4266df329883f6b1320acc395
Salem Abebe <salemabebe@yahoo.com>
2022-09-27 16:40:23 -0400

    comment out service reducer

--- src/ReduxStore/store.js
+++ src/ReduxStore/store.js
@@ -3,15 +3,15 @@ import { configureStore } from "@reduxjs/toolkit";
 //slices
 import aboutReducer from "./slices/AboutSlice";
 import heroReducer from "./slices/HeroSlice";
-import servicesReducer from "./slices/ServicesSlice";
-import testimonialReducer from "./slices/testimonialSlice";
+// import servicesReducer from "./slices/ServicesSlice";
+import testimonialReducer from "./slices/TestimonialSlice";
 import notificationReducer from "./slices/NotificationSlice";
 
 const store = configureStore({
   reducer: {
     about: aboutReducer,
     hero: heroReducer,
-    services: servicesReducer,
+    // services: servicesReducer,
     testimonial: testimonialReducer,
     notification: notificationReducer,
   },
