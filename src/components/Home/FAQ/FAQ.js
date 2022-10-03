58304266f409696915dd940ba851ea9c1337db34
Salem Abebe <salemabebe@yahoo.com>
2022-10-03 18:10:37 -0400

    Draft FAQ component

--- src/components/Home/FAQ/FAQ.js
+++ src/components/Home/FAQ/FAQ.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./FAQ.scss";
 
 function FAQ() {
   return (
-    <div>FAQ</div>
-  )
+    <div className="FAQ-home">
+      <div className="FAQ-home-title">
+        <h1>FAQ</h1>
+      </div>
+
+      <div className="FAQ-home-wrapper">
+        <div className="question-answer">
+          <div className="question-wrapper">
+            <p className="question"></p>
+            <p>
+              <hr className="1" />
+              <hr />
+            </p>
+          </div>
+        </div>
+        <div className="answer">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default FAQ
+export default FAQ;
