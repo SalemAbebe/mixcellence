ded4e41c0adf414537c41b2ea6bfe46572235945
Salem Abebe <salemabebe@yahoo.com>
2022-10-03 10:28:28 -0400

    spelling correction on "testimonial-home" class name

--- src/components/Home/Testimonial/Testimonial.js
+++ src/components/Home/Testimonial/Testimonial.js
@@ -8,17 +8,17 @@ function Testimonial() {
         <h1>What Some have to say</h1>
       </div>
       <div className="testimonials-home-wrapper">
-        <div className="testimonials-home">
+        <div className="testimonial-home">
           <div className="name">xx</div>
           <div className="about-mixcellence"></div>
           <div className="address"></div>
         </div>
-        <div className="testimonials-home">
+        <div className="testimonial-home">
           <div className="name"></div>
           <div className="about-mixcellence"></div>
           <div className="address"></div>
         </div>
-        <div className="testimonials-home">
+        <div className="testimonial-home">
           <div className="name"></div>
           <div className="about-mixcellence"></div>
           <div className="address"></div>
