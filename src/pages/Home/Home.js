e3df64aff16520e669eb0769bba24fc97c771c8e
Salem Abebe <salemabebe@yahoo.com>
2022-10-03 18:11:19 -0400

    Render Events and FAQ component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Events from "../../components/Home/Events/Events";
+import FAQ from "../../components/Home/FAQ/FAQ";
 import Testimonial from "../../components/Home/Testimonial/Testimonial";
 
 function Home() {
@@ -6,6 +8,8 @@ function Home() {
     <div>
       Home
       <Testimonial />
+      <Events />
+      <FAQ />
     </div>
   );
 }
