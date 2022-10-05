c4ba04e1f827b7ee989b5b8834a401689a1b1183
aim4dabush <edarasato@gmail.com>
2022-10-05 14:18:19 -0700

    finished design and functionality of FAQ component

--- src/components/Home/FAQ/FAQ.js
+++ src/components/Home/FAQ/FAQ.js
@@ -1,26 +1,53 @@
 import React from "react";
+
+//data
+import { faqData } from "./FAQData";
+
+//redux
+import { useDispatch, useSelector } from "react-redux";
+import { FAQActions } from "../../../ReduxStore/slices/FAQSlice";
+
+//styles
 import "./FAQ.scss";
 
 function FAQ() {
+  const dispatch = useDispatch();
+  const showAnswer = useSelector((state) => state.FAQ.showAnswer);
+
+  const showAnswerHandler = (index) => {
+    if (showAnswer !== index) {
+      dispatch(FAQActions.handleShowAnswer(index));
+    }
+  };
   return (
     <div className="FAQ-home">
-      <div className="FAQ-home-title">
-        <h1>FAQ</h1>
-      </div>
-
-      <div className="FAQ-home-wrapper">
-        <div className="question-answer">
-          <div className="question-wrapper">
-            <p className="question"></p>
-            <div>
-              <hr className="1" />
-              <hr />
+      <h2 className="FAQ-header">FAQ</h2>
+      <div className="FAQ-wrapper">
+        {faqData.map((item, index) => {
+          return (
+            <div key={item.id} className="faq-section">
+              <div className="faq-control">
+                <h5 className="faq-title">{item.title}</h5>
+                <div
+                  className="symbol"
+                  onClick={() => showAnswerHandler(index)}
+                >
+                  {showAnswer === index ? "-" : "+"}
+                </div>
+              </div>
+              <span />
+              {showAnswer === index ? (
+                <p
+                  className={
+                    "faq-text " + (showAnswer === index ? "active" : "")
+                  }
+                >
+                  {item.text}
+                </p>
+              ) : null}
             </div>
-          </div>
-        </div>
-        <div className="answer">
-          <p></p>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Home/FAQ/FAQData.js
+++ src/components/Home/FAQ/FAQData.js
@@ -2,10 +2,21 @@ export const faqData = [
   {
     id: 0,
     title: "Where are located? What areas do you serve?",
-    text: "We are based out of Monterey California. The primary regions that we serve include the Central Coast Area, and Surounding Counties, Sacramento and Monterey. However, we are willing to travel throughout California, if scheduling allows.",
+    text: "We are based out of Monterey California. The primary regions that we serve include the Central Coast Area, and Surrounding Counties, Sacramento and Monterey. However, we are willing to travel throughout California, if scheduling allows.",
   },
   {
     id: 1,
-    // title = ""
+    title: "What are your fees?",
+    text: "Our packages are customized based on a variety of factors, including location, hours, number of guests, number of bartenders, and the services you are requesting. Please start by taking a look at the example packages on our Services page. Once you have an idea of the services you are looking for, please email us through our Contact page to discuss your custom package and quote.",
+  },
+  {
+    id: 2,
+    title: "Do you provide the alcohol?",
+    text: "No, our packages do not include any alcohol. You will need to purchase the alcohol from a separate vendor (store, your caterer, your venue), which will then be mixed and served by our bartenders. However, all of the prep work leading up to your alcohol purchase will be done by us--we will help you to decide what you need to order, how much you need to order, and when/where it should arrive.",
+  },
+  {
+    id: 3,
+    title: "What types of events are you able to serve?",
+    text: 'We are able to serve at private events. This includes weddings, birthday parties, baby or bridal showers, corporate parties, fundraisers, etc. For public events, it is the responsibility of the client to secure necessary permitting. Events must not be operating as a "cash bar", as we are unable to sell alcohol from our bar.',
   },
 ];
