e6e37ae79a56016ea4e0f8b5fbf6c68679cda25d
aim4dabush <edarasato@gmail.com>
2022-10-05 15:31:50 -0700

    added navigation on login form

--- src/components/AdminLogin/Login/LoginForm/LoginForm.js
+++ src/components/AdminLogin/Login/LoginForm/LoginForm.js
@@ -5,16 +5,26 @@ import LoginEmail from "./LoginEmail";
 import LoginPassword from "./LoginPassword";
 import LoginSubmitButton from "./LoginSubmitButton";
 
+//react router
+import { useNavigate } from "react-router-dom";
+
 //styles
 import "./LoginForm.scss";
 
 function LoginForm() {
+  const navigate = useNavigate();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    navigate("/admin", { replace: true });
+  };
+
   return (
     <div className="login-form-container">
       <div>
         <h1>Welcome Roel!</h1>
       </div>
-      <form className="login-form">
+      <form className="login-form" onSubmit={onSubmitHandler}>
         <LoginEmail />
         <LoginPassword />
         <LoginSubmitButton />
